d5595c2d157416642d0535ef36f4323ca2e72891
kashkoin-foundation <36289897+kashkoin-foundation@users.noreply.github.com>
2018-07-11 08:06:39 +0530

    update link changed

--- src/Update.cpp
+++ src/Update.cpp
@@ -89,7 +89,7 @@ void Updater::replyFinished (QNetworkReply *reply)
          if (ourVersion < remoteVersion) {
 
              if (QMessageBox::warning(nullptr, QObject::tr("New version available"), QObject::tr("There is update available.\nDo you want to go to download page?"), QMessageBox::Ok, QMessageBox::Cancel) == QMessageBox::Ok) {
-                 QString link = "http://kashkoin.ml/downloads.html";
+                 QString link = "http://kashkoin.ml/#wallet";
                  QDesktopServices::openUrl(QUrl(link));
              }
 
